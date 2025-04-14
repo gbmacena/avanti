d2925970e2005664bac70da680cdf03eb47ea39f
Gabriel Macena <gabriel.macena10@hotmail.com>
2025-04-13 22:44:32 -0300

    feat: Feature de busca

--- script/index.js
+++ script/index.js
@@ -151,3 +151,77 @@ document.addEventListener("DOMContentLoaded", function () {
     icon.classList.add("fa-bars");
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const searchInput = document.getElementById("search-input");
+  const searchButton = document.getElementById("search-button");
+  const clearButton = document.getElementById("clear-button");
+  const searchResult = document.getElementById("search-result");
+  const searchResultText = document.getElementById("search-result-text");
+
+  const mobileSearchInput = document.getElementById("mobile-search-input");
+  const mobileSearchButton = document.getElementById("mobile-search-button");
+  const mobileClearButton = document.getElementById("mobile-clear-button");
+  const mobileSearchResult = document.getElementById("mobile-search-result");
+  const mobileSearchResultText = document.getElementById(
+    "mobile-search-result-text"
+  );
+
+  function performSearch(input, result, resultText, clearButton) {
+    const searchText = input.value.trim();
+
+    if (searchText.trim()) {
+      resultText.textContent = `Você buscou por: '${searchText}'`;
+      result.classList.remove("hidden");
+      clearButton.classList.remove("hidden");
+    }
+  }
+
+  function clearSearch(input, result, clearButton) {
+    input.value = "";
+    result.classList.add("hidden");
+    clearButton.classList.add("hidden");
+  }
+
+  searchButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    performSearch(searchInput, searchResult, searchResultText, clearButton);
+  });
+
+  searchInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      performSearch(searchInput, searchResult, searchResultText, clearButton);
+    }
+  });
+
+  clearButton.addEventListener("click", function () {
+    clearSearch(searchInput, searchResult, clearButton);
+  });
+
+  mobileSearchButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    performSearch(
+      mobileSearchInput,
+      mobileSearchResult,
+      mobileSearchResultText,
+      mobileClearButton
+    );
+  });
+
+  mobileSearchInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      performSearch(
+        mobileSearchInput,
+        mobileSearchResult,
+        mobileSearchResultText,
+        mobileClearButton
+      );
+    }
+  });
+
+  mobileClearButton.addEventListener("click", function () {
+    clearSearch(mobileSearchInput, mobileSearchResult, mobileClearButton);
+  });
+});
